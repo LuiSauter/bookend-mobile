2fa2de069be97e8a236d3333524a65b82393b355
Luis Gabriel <janco7249@gmail.com>
2022-01-31 22:15:53 -0400

    add eslint and prettierrc

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': 0,
+    'jsx-quotes': ['error', 'prefer-single'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
