94b3e30dca35608093195d167b7b0a510d9a2dcc
Luis Gabriel <janco7249@gmail.com>
2022-02-08 20:12:43 -0400

    update modal to image full screen

--- screens/ModalImageScreen.js
+++ screens/ModalImageScreen.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
-import { Image, StyleSheet, ImageBackground, View } from 'react-native'
+import { Image, StyleSheet, ImageBackground, View, StatusBar, SafeAreaView } from 'react-native'
 import React, { useEffect } from 'react'
-import Layout from '../components/Layout'
 import { useToggle } from '../hooks/useToggle'
 import Icon from 'react-native-vector-icons/AntDesign'
 
@@ -11,41 +10,70 @@ const ModalImageScreen = ({ route, navigation }) => {
 
   useEffect(() => {
     let cleanup = true
-    if (cleanup) handleAddImage(image)
-
+    if (cleanup) {
+      handleAddImage(image)
+    }
     return () => {
       cleanup = false
     }
   }, [image])
 
+  const colors = [
+    '#2666CF',
+    '#8A39E1',
+    '#BB6464',
+    '#219F94',
+    '#F94892',
+    '#035397',
+    '#FFC600',
+    '#502064',
+    '#4A3F35',
+    '#BE3144',
+    '#21325E',
+    '#F76E11',
+  ]
+
+  const colorRandom = colors[Math.floor(Math.random() * colors.length)]
+
   return (
-    <Layout>
+    <SafeAreaView style={{ flex: 1, justifyContent: 'space-between' }}>
+      <StatusBar
+        barStyle='default'
+        animated={true}
+        showHideTransition={'none'}
+        backgroundColor={colorRandom}
+        translucent={true}
+        hidden //not found in android
+      />
       <ImageBackground
+        // onLoad={(e) => {
+        // }}
+        // imageStyle={{ tintColor: colorRandom }}
         style={{
           width: '100%',
           height: '100%',
           flex: 1,
-          alignItems: 'center',
           justifyContent: 'center',
         }}
         source={{ uri: image }}
-        blurRadius={150}
+        blurRadius={100}
       >
         <View style={styles.backIcon}>
           <Icon.Button
             backgroundColor='transparent'
             borderRadius={50}
             name='arrowleft'
+            color={'#aaa'}
             onPress={() => navigation.goBack()}
             size={27}
             padding={5}
             iconStyle={{ marginRight: 1 }}
-            underlayColor='#0004'
+            underlayColor='#555a'
           />
         </View>
         <Image style={styles.image} source={{ uri: image }} />
       </ImageBackground>
-    </Layout>
+    </SafeAreaView>
   )
 }
 
@@ -60,12 +88,9 @@ const styles = StyleSheet.create({
     color: '#fff',
     flex: 1,
     alignItems: 'flex-start',
-    // flexShrink: 1,
     top: 0,
     left: 0,
-    // height: 30,
-    // width: 30,
-    margin: 10,
+    margin: 16,
   },
 })
 
