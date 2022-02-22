d306731eb59ec0f86c7a9f6d3d77ddefbe1d09d4
Luis Gabriel <janco7249@gmail.com>
2022-02-21 22:32:46 -0400

    add colors random

--- config/colors.js
+++ config/colors.js
@@ -10,3 +10,18 @@ export const colors = {
   textWhite: '#F5FDFF',
   TextGray: '#4D5B66',
 }
+
+export const colorsRandom = [
+  '#2666CF',
+  '#8A39E1',
+  '#BB6464',
+  '#219F94',
+  '#F94892',
+  '#035397',
+  '#FFC600',
+  '#502064',
+  '#4A3F35',
+  '#BE3144',
+  '#21325E',
+  '#F76E11',
+]
