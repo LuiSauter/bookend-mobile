2ad8c18d363dcf62b400829b3996aa8c59cfc499
Luis Gabriel <janco7249@gmail.com>
2022-02-24 23:45:21 -0400

    added context state to dark theme

--- context/toggleContext.js
+++ context/toggleContext.js
@@ -1,18 +1,24 @@
 /* eslint-disable react/prop-types */
+import { useColorScheme } from 'react-native'
 import React, { createContext, useState } from 'react'
 
 export const ToggleContext = createContext({})
 
 export const ToggleStateProvider = ({ children }) => {
+  const scheme = useColorScheme()
   const [modalVisible, setModalVisible] = useState(false)
+  const [darkTheme, setDarkTheme] = useState(scheme === 'dark')
 
   const handleModalVisible = () => setModalVisible(!modalVisible)
+  const handleDarkTheme = () => setDarkTheme(!darkTheme)
 
   return (
     <ToggleContext.Provider
       value={{
         modalVisible,
         handleModalVisible,
+        darkTheme,
+        handleDarkTheme,
       }}
     >
       {children}
--- hooks/useToggle.js
+++ hooks/useToggle.js
@@ -2,7 +2,7 @@ import { useContext } from 'react'
 import { ToggleContext } from '../context/toggleContext'
 
 export const useToggle = () => {
-  const { modalVisible, handleModalVisible } = useContext(ToggleContext)
+  const { modalVisible, handleModalVisible, darkTheme, handleDarkTheme } = useContext(ToggleContext)
 
-  return { modalVisible, handleModalVisible }
+  return { modalVisible, handleModalVisible, darkTheme, handleDarkTheme }
 }
