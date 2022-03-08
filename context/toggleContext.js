ffd962827890ba1e5bec1f7a00b6e1cb22bdbb77
Luis Gabriel <janco7249@gmail.com>
2022-03-07 21:30:57 -0400

    [refactor] changes default theme to darkmode

--- context/toggleContext.js
+++ context/toggleContext.js
@@ -1,13 +1,11 @@
 /* eslint-disable react/prop-types */
-import { useColorScheme } from 'react-native'
 import React, { createContext, useState } from 'react'
 
 export const ToggleContext = createContext({})
 
 export const ToggleStateProvider = ({ children }) => {
-  const scheme = useColorScheme()
   const [modalVisible, setModalVisible] = useState(false)
-  const [darkTheme, setDarkTheme] = useState(scheme === 'dark')
+  const [darkTheme, setDarkTheme] = useState(true)
   const [word, setWord] = useState('')
 
   const handleModalVisible = () => setModalVisible(!modalVisible)
