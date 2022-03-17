452e0b779b7996273f7a7d017d48a75c9dd15c94
Luis Gabriel <janco7249@gmail.com>
2022-03-17 11:55:09 -0400

    changes metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,10 @@
-const { getDefaultConfig } = require('metro-config')
-const { resolver: defaultResolver } = getDefaultConfig.getDefaultValues()
-exports.resolver = {
-  ...defaultResolver,
-  sourceExts: [...defaultResolver.sourceExts, 'cjs'],
-}
+const { getDefaultConfig } = require('expo/metro-config')
+
+const config = getDefaultConfig(__dirname)
+
+// Remove all console logs in production...
+config.transformer.minifierConfig.compress.drop_console = true
+config.transformer.minifierPath = 'metro-minify-terser'
+config.resolver.sourceExts.push('cjs')
+
+module.exports = config
